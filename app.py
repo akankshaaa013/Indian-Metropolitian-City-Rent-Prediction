f0799916b94f908e71ed1aafc19861d1ff6d975e
akankshaaa013 <gakanksha013@gmail.com>
2024-07-14 22:32:56 +0530

    Commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from django.shorcuts import render
+# from django.shorcuts import render
 import pickle
 from flask import Flask, request, app, jsonify, url_for, render_template
 import numpy as np
@@ -17,4 +17,13 @@ def home():
 def predict_api():
     data = request.json['data']
     print(data)
+    print(np.array(list(data.values())))
+    
+    # ['BHK', 'Size', Area Type', 'City', Furnishing Status', Tenant Preffered', Bathroom', Point of Contact', Total Floor']
+    
+    # Loading all the pickle files.
+    scalar = pickle.load(open('scaler.pkl','rb'))
+    regression = pickle.load(open('regmodel.pkl','rb'))
+    poly_transformation = pickle.load(open('poly_regs.pkl','rb'))
+    
     
